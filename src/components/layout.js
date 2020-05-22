994799b5233013a0611dbfb0764de7dda09310c8
Andy Pickle <pickleat@gmail.com>
2020-05-22 15:54:29 -0500

    finished formattingNestedHTMLElements.md post

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ import FlyoutMenu from "../components/FlyoutMenu.js"
 
 const Layout = ({children}) => {
     return(
-        <div className='mx-auto bg-navy bg-center bg-repeat' style={{backgroundImage: `url(${background})`}}>
+        <div className='mx-auto bg-navy bg-contain bg-repeat' style={{backgroundImage: `url(${background})`}}>
             <Link className='flex items-center hover:text-blue-600 hover:underline pt-4 pl-4' to='/'>
                 <svg className='fill-current text-yellow-100 h-6 w-6' viewBox="0 0 51 50" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20.1954 50H7.57329V25H0L25.2443 0L50.4886 25H42.9153V50H30.2932V35H20.1954V50Z" /></svg>
             </Link>  
