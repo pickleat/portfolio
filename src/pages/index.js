3eed34173a8bc1f697bd0b32c39a40cc66ca9e38
Andy Pickle <pickleat@gmail.com>
2020-05-25 07:50:40 -0500

    hiding the "About" on larger screens, but leaving it for smaller ones

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,28 +31,28 @@ export default ({data}) => {
             <Heading title='Andy Pickle'/>
             <div className='bg-yellow-100 text-navy px-10 pb-20 sm:pb-20 w-full md:max-w-6xl'>
                 <H2Heading text='frontend developer'/>
-                                            <div className='flex flex-col sm:hidden font-bold text-2xl mt-2'>
-                                <div className='flex justify-around'>
-                                    <button onClick={() => {showLine(1)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/about'>
-                                        <svg id='button-1' className='fill-current h-6 w-6' viewBox="0 0 50 47"xmlns="http://www.w3.org/2000/svg"><path d="M10 26.4462H5C3.67392 26.4462 2.40215 25.9077 1.46447 24.9492C0.526784 23.9907 0 22.6906 0 21.3351V11.1128C0 8.30174 2.25 6.00174 5 6.00174H10V0.890625H45V26.4462C45 29.1573 43.9464 31.7573 42.0711 33.6744C40.1957 35.5914 37.6522 36.6684 35 36.6684H20C17.3478 36.6684 14.8043 35.5914 12.9289 33.6744C11.0536 31.7573 10 29.1573 10 26.4462ZM10 21.3351V11.1128H5V21.3351H10ZM5 41.7795V39.224H50V41.7795L40 46.8906H15L5 41.7795Z" /></svg>
-                                        <span  className='hidden'>About</span>
-                                    </button>                        
-                                    <button onClick={() => {showLine(2)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/projects'>
-                                        <svg id='button-2' className='fill-current h-6 w-6' viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.1675 30.3573H5V50.3573H0V0.3573H30L30.8325 5.3573H50L42.5 20.3573L50 35.3573H20L19.1675 30.3573Z" /></svg>
-                                        <span  className='hidden'>Projects</span>
-                                    </button>
-                                {/* </div> */}
-                                {/* <div className='flex justify-around'> */}
-                                    <button onClick={() => {showLine(3)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/blog'>
-                                        <svg id='button-3' className='fill-current h-6 w-6' viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10.4274V45.4274H40V30.4274L45 25.4274V50.4274H0V5.42737H25L20 10.4274H5ZM30.75 9.67737L40.75 19.6774L20 40.4274H10V30.4274L30.75 9.67737ZM34.25 6.17737L40 0.427368L50 10.4274L44.25 16.1774L34.25 6.17737Z" /></svg>
-                                        <span  className='hidden'>Blog</span>
-                                    </button>                        
-                                    <button onClick={() => {showLine(4)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/contact'>
-                                        <svg id='button-4' className='fill-current h-6 w-6' viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M34 34.2152C32.2758 36.022 30.0489 37.2698 27.6077 37.7969C25.1664 38.324 22.6231 38.1062 20.307 37.1716C17.9909 36.2371 16.0086 34.6289 14.6167 32.5552C13.2249 30.4815 12.4875 28.0377 12.5 25.5402C12.5 23.2188 13.1464 20.9432 14.3669 18.9685C15.5873 16.9938 17.3335 15.398 19.4098 14.3598C21.4862 13.3217 23.8106 12.8822 26.1226 13.0907C28.4346 13.2992 30.6429 14.1473 32.5 15.5402V13.0402H37.5V29.2902C37.5 30.2847 37.8951 31.2386 38.5984 31.9418C39.3016 32.6451 40.2555 33.0402 41.25 33.0402C42.2446 33.0402 43.1984 32.6451 43.9017 31.9418C44.6049 31.2386 45 30.2847 45 29.2902V25.5402C44.9969 21.1891 43.575 16.9578 40.9498 13.4879C38.3247 10.018 34.6396 7.49877 30.4535 6.31236C26.2673 5.12595 21.8084 5.33705 17.7531 6.91363C13.6977 8.49022 10.2671 11.3463 7.98156 15.0487C5.696 18.7511 4.68017 23.0978 5.08813 27.4297C5.49608 31.7616 7.30557 35.8423 10.2422 39.0529C13.1788 42.2635 17.0823 44.4288 21.3607 45.2206C25.6391 46.0123 30.059 45.3873 33.95 43.4402L36.2 47.9152C31.3372 50.3519 25.8125 51.1363 20.4636 50.1493C15.1148 49.1623 10.2337 46.4579 6.56084 42.4462C2.88794 38.4344 0.623577 33.3343 0.1112 27.9194C-0.401177 22.5044 0.866391 17.0701 3.72164 12.4407C6.57689 7.81124 10.864 4.23928 15.9328 2.26657C21.0016 0.293863 26.5755 0.0280453 31.809 1.50944C37.0425 2.99083 41.6501 6.1386 44.9329 10.4753C48.2158 14.812 49.9948 20.1011 50 25.5402H49.55H49.975V29.2902C49.9806 31.1657 49.3834 32.9934 48.2715 34.5038C47.1596 36.0141 45.5918 37.1273 43.7992 37.679C42.0067 38.2308 40.0843 38.192 38.3155 37.5683C36.5467 36.9447 35.0251 35.7692 33.975 34.2152H34ZM25 33.0402C26.9891 33.0402 28.8968 32.25 30.3033 30.8435C31.7098 29.437 32.5 27.5293 32.5 25.5402C32.5 23.5511 31.7098 21.6434 30.3033 20.2369C28.8968 18.8304 26.9891 18.0402 25 18.0402C23.0109 18.0402 21.1032 18.8304 19.6967 20.2369C18.2902 21.6434 17.5 23.5511 17.5 25.5402C17.5 27.5293 18.2902 29.437 19.6967 30.8435C21.1032 32.25 23.0109 33.0402 25 33.0402Z" /></svg>
-                                        <span  className='hidden'>Contact</span>
-                                    </button>                        
-                                </div>
-                            </div>
+                <div className='flex flex-col sm:hidden font-bold text-2xl mt-2'>
+                    <div className='flex justify-around'>
+                        <button onClick={() => {showLine(1)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/about'>
+                            <svg id='button-1' className='fill-current h-6 w-6' viewBox="0 0 50 47"xmlns="http://www.w3.org/2000/svg"><path d="M10 26.4462H5C3.67392 26.4462 2.40215 25.9077 1.46447 24.9492C0.526784 23.9907 0 22.6906 0 21.3351V11.1128C0 8.30174 2.25 6.00174 5 6.00174H10V0.890625H45V26.4462C45 29.1573 43.9464 31.7573 42.0711 33.6744C40.1957 35.5914 37.6522 36.6684 35 36.6684H20C17.3478 36.6684 14.8043 35.5914 12.9289 33.6744C11.0536 31.7573 10 29.1573 10 26.4462ZM10 21.3351V11.1128H5V21.3351H10ZM5 41.7795V39.224H50V41.7795L40 46.8906H15L5 41.7795Z" /></svg>
+                            <span  className='hidden'>About</span>
+                        </button>                        
+                        <button onClick={() => {showLine(2)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/projects'>
+                            <svg id='button-2' className='fill-current h-6 w-6' viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19.1675 30.3573H5V50.3573H0V0.3573H30L30.8325 5.3573H50L42.5 20.3573L50 35.3573H20L19.1675 30.3573Z" /></svg>
+                            <span  className='hidden'>Projects</span>
+                        </button>
+                    {/* </div> */}
+                    {/* <div className='flex justify-around'> */}
+                        <button onClick={() => {showLine(3)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/blog'>
+                            <svg id='button-3' className='fill-current h-6 w-6' viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 10.4274V45.4274H40V30.4274L45 25.4274V50.4274H0V5.42737H25L20 10.4274H5ZM30.75 9.67737L40.75 19.6774L20 40.4274H10V30.4274L30.75 9.67737ZM34.25 6.17737L40 0.427368L50 10.4274L44.25 16.1774L34.25 6.17737Z" /></svg>
+                            <span  className='hidden'>Blog</span>
+                        </button>                        
+                        <button onClick={() => {showLine(4)}} className='flex items-center hover:text-blue-600 hover:underline pt-2' to='/contact'>
+                            <svg id='button-4' className='fill-current h-6 w-6' viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M34 34.2152C32.2758 36.022 30.0489 37.2698 27.6077 37.7969C25.1664 38.324 22.6231 38.1062 20.307 37.1716C17.9909 36.2371 16.0086 34.6289 14.6167 32.5552C13.2249 30.4815 12.4875 28.0377 12.5 25.5402C12.5 23.2188 13.1464 20.9432 14.3669 18.9685C15.5873 16.9938 17.3335 15.398 19.4098 14.3598C21.4862 13.3217 23.8106 12.8822 26.1226 13.0907C28.4346 13.2992 30.6429 14.1473 32.5 15.5402V13.0402H37.5V29.2902C37.5 30.2847 37.8951 31.2386 38.5984 31.9418C39.3016 32.6451 40.2555 33.0402 41.25 33.0402C42.2446 33.0402 43.1984 32.6451 43.9017 31.9418C44.6049 31.2386 45 30.2847 45 29.2902V25.5402C44.9969 21.1891 43.575 16.9578 40.9498 13.4879C38.3247 10.018 34.6396 7.49877 30.4535 6.31236C26.2673 5.12595 21.8084 5.33705 17.7531 6.91363C13.6977 8.49022 10.2671 11.3463 7.98156 15.0487C5.696 18.7511 4.68017 23.0978 5.08813 27.4297C5.49608 31.7616 7.30557 35.8423 10.2422 39.0529C13.1788 42.2635 17.0823 44.4288 21.3607 45.2206C25.6391 46.0123 30.059 45.3873 33.95 43.4402L36.2 47.9152C31.3372 50.3519 25.8125 51.1363 20.4636 50.1493C15.1148 49.1623 10.2337 46.4579 6.56084 42.4462C2.88794 38.4344 0.623577 33.3343 0.1112 27.9194C-0.401177 22.5044 0.866391 17.0701 3.72164 12.4407C6.57689 7.81124 10.864 4.23928 15.9328 2.26657C21.0016 0.293863 26.5755 0.0280453 31.809 1.50944C37.0425 2.99083 41.6501 6.1386 44.9329 10.4753C48.2158 14.812 49.9948 20.1011 50 25.5402H49.55H49.975V29.2902C49.9806 31.1657 49.3834 32.9934 48.2715 34.5038C47.1596 36.0141 45.5918 37.1273 43.7992 37.679C42.0067 38.2308 40.0843 38.192 38.3155 37.5683C36.5467 36.9447 35.0251 35.7692 33.975 34.2152H34ZM25 33.0402C26.9891 33.0402 28.8968 32.25 30.3033 30.8435C31.7098 29.437 32.5 27.5293 32.5 25.5402C32.5 23.5511 31.7098 21.6434 30.3033 20.2369C28.8968 18.8304 26.9891 18.0402 25 18.0402C23.0109 18.0402 21.1032 18.8304 19.6967 20.2369C18.2902 21.6434 17.5 23.5511 17.5 25.5402C17.5 27.5293 18.2902 29.437 19.6967 30.8435C21.1032 32.25 23.0109 33.0402 25 33.0402Z" /></svg>
+                            <span  className='hidden'>Contact</span>
+                        </button>                        
+                    </div>
+                </div>      
                 <div className='flex'>
                     <div className='hidden sm:flex flex-col w-1/3 justify-around py-8'>
                         <Link className='flex items-center hover:underline' onMouseOver={() => {showLine(1)}} onFocus={() => {showLine(1)}} to="/about">
@@ -82,7 +82,7 @@ export default ({data}) => {
 
                         <div id='text-1' className='pl-2'>
                             <h3 className='text-center text-2xl font-bold'>
-                                <Link className="hover:text-blue-600 underline" to='/about'>About</Link>
+                                <Link className="hover:text-blue-600 underline sm:hidden" to='/about'>About</Link>
                             </h3>
                             <h4 className='text-center text-2xl font-bold'><span role='img' aria-label='hand-wave-emoji'>👋🏻</span> Hi, I'm Andy! </h4>
                             <p>
@@ -102,7 +102,7 @@ export default ({data}) => {
                             <h3 className='text-center text-2xl font-bold'>
                                 <Link className="hover:text-blue-600 underline" to='/projects'>Projects</Link>
                             </h3>
-                            <p className='pb-2'>Here's my most recent projects.</p>
+                            <p className='pb-2'>Here are my most recent projects.</p>
                             <ul className='sm:list-inside list-disc flex-grow'>
                                 {projects.map(({node}, index) => (
                                     <li className='sm:pl-2' key={node.id}>
@@ -116,7 +116,7 @@ export default ({data}) => {
                             <h3 className='text-center text-2xl font-bold'>
                                 <Link className="hover:text-blue-600 underline" to='/blog'>Blog</Link>
                             </h3>
-                            <p className='pb-2'>Here's my most recent posts.</p>
+                            <p className='pb-2'>Here are my most recent posts.</p>
                             <ul className='sm:list-inside list-disc flex-grow'>
                             {blogs.map(({ node }, index) => (
                                 <li className='sm:pl-2' key={node.id}>
