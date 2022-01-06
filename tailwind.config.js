971fc7d5f0a4a96373b6d35e03bb1ff1eeb31e29
Andy Pickle <pickleat@gmail.com>
2022-01-06 10:10:27 -0600

    updating the purge to content per new version needs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 const { colors, fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      './src/components/*.js',
-      './src/pages/*.js',
-      './src/templates/*.js',
-    ]
-  },
+  content: [
+    './src/components/*.js',
+    './src/pages/*.js',
+    './src/templates/*.js',
+  ],
   theme: {
     extend: {
       colors: {
