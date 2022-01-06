5ccd5b90eb8fdfea94fe493d8f0635e0b54e668f
Andy Pickle <pickleat@gmail.com>
2022-01-06 10:11:37 -0600

    updating my about and adding some courses/conferences

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import InnerLayout from "../components/innerLayout"
 import Heading from "../components/H1HeadingAnd3Dots"
 import H2Heading from "../components/H2Heading"
 
-export default ({data}) => {
+const About = ({data}) => {
 
     return(
         <Layout>
@@ -14,7 +14,7 @@ export default ({data}) => {
                 <H2Heading text="a little about me..." />
                 <div className='sm:flex pt-2'>
                     <h3 className='font-black lowercase tracking-tight text-3xl'>tl;dr</h3>
-                    <p className="sm:pl-4">I'm a self-taught, front-end web developer from Little Rock, Arkansas. I enjoy writing React and Tailwind, but I've worked with lots of other tech.</p>
+                    <p className="sm:pl-4">I'm a self-taught, front-end/UI Engineer from Little Rock, Arkansas. I use Vue professionally for work, but I enjoy writing React as well. I'm a JavaScript nerd at heart.</p>
                 </div>
                 <div className='py-4 sm:p-4'>
                     <hr className='text-navy border-top border-solid border-navy border-2' />
@@ -26,14 +26,13 @@ export default ({data}) => {
                         After finishing High School, I went to a small liberal-arts university in Nashville (Go Bisons!). 
                     </p>
                     <p className="pt-4">
-                        I started as a 
-                        Mechanical Engineering major, but first semester Calculus 2 kicked my butt. Even though I passed the class, I thought because it didn't come easy to me that perhaps I wasn't cut out for a technical major.
+                        I started as a Mechanical Engineering major, but first semester Calculus 2 kicked my butt. Even though I passed the class, I thought because it didn't come easy to me that perhaps I wasn't cut out for a technical major.
                         Looking back I wish someone had pushed me to carry on, and realize that things are difficult. Learning is a skill as much as anything, really I needed a push to have a growth mindset. 
                         However I ended up switching to a less technical major and studied Theology & Ministry with a minor in Greek. I enjoyed those very much, but seeing as I don't use them much anymore it 
                         does feel like a bit of wasted resources. Experience-wise, I wouldn't trade it for anything, just wish I would've chosen computer science then... oh well.
                     </p>
                     <p className="pt-4">
-                        After college, including 4 years of dating long distance, I married my high school sweetheart, Kaylee. She is still the greatest joy of my life. And recently picked up a hobby baking cakes!
+                        After college, including 4 years of dating long distance, I married my high school sweetheart, Kaylee. She is the greatest joy of my life.
                     </p>
                     <p className="pt-4">
                         Eventually, I found myself in the specialty coffee industry, as a barista, roaster, roasting manager, and finally co-owner of a small coffee company.
@@ -46,21 +45,29 @@ export default ({data}) => {
                         However sadly, that startup wound down and I found it difficult to find another dev job quickly. 
                     </p>
                     <p className="pt-4">
-                        I took a job doing technical project managment at a design firm called <a className='blue-link' href="https://few.io">Few</a>. Most of our projects are websites or webapps for clients, with a few React Native projects mixed in. 
-                        I've managed the gamut of those type of projects, and have a great feel of how the development life-cycle works. Scoping features and delivering on them, from a management point of view, although I've gotten a few PRs merged in my time at Few. 
+                        I took a job doing technical project management at a design firm called <a className='blue-link' href="https://few.io">Few</a>. Most of our projects are websites or web apps for clients, with a few React Native projects mixed in. 
+                        I've managed the gamut of those type of projects, and have a great feel of how the development life-cycle works. Scoping features and delivering on them. Eventually I transitioned over to front-end development and did that for several months full time. 
                     </p>
                     <p className='pt-4'>
-                        Now? Well currently I'm building <a className='blue-link' href='https://protege.dev'>Protege.dev</a> with my good friend <a className='blue-link' href='https://twitter.com/drewclemcr8'>Drew Clements</a>.
-                        I've been spending most of my dev time in React. Redesigning my portfolio in Figma, and building it on Gatsby and Tailwind. 
-                        If you're interested in hiring me, reach out on my <Link className='blue-link' to='contact'>Contact Page.</Link>
+                        Early in the pandemic I started working on <a className='blue-link' href='https://protege.dev'>Protege.dev</a> with my good friend <a className='blue-link' href='https://twitter.com/drewclemcr8'>Drew Clements</a>.
+                        We built it with React + TailwindCSS, but eventually started taking on too much freelance work to continue contributing. 
+                    </p>
+                    <p className='pt-4'>
+                        In early 2021 my wife and I welcomed our second child into our family! She's amazing. We love our little family, but with that growth, I took a backseat from side-projects and freelancing for most of the year.
+                    </p>
+                    <p className='pt-4'>
+                        In late 2021 I took a job as a UI Software Engineer at <a className='blue-link' href='https://threeflow.com'>ThreeFlow</a> which is in the insurance tech industry. And launched a large freelance client site <a className='blue-link' href='https://ForeverYoungAdult.com'>ForeverYoungAdult.com</a>!
+                    </p>
+                    <p className='pt-4'>
+                        This year I'm focusing on getting back into some side-projects, writing and growing as a developer! You can read more about that on my post: <Link className='blue-link' to='/posts/firstPostOf2022/'>"Goals for 2022"</Link>
                     </p>
                 </div>
                 <div className='p-4'>
                     <hr className='text-navy border-top border-solid border-navy border-2' />
                 </div>
                 <div>
-                    <h5 className='font-black lowercase tracking-tight text-3xl' id='certifications'>Certifications and Courses</h5>
-                    <div className='sm:flex items-center'>
+                    <h5 className='font-black lowercase tracking-tight text-3xl' id='certifications'>Certifications, Courses, and Conferences</h5>
+                    <div className='flex-col items-center'>
                         <span className='font-black lowercase tracking-tight text-2xl'>2019</span>
                         <ul className='sm:pl-4 flex flex-col'>
                             <li>
@@ -75,6 +82,21 @@ export default ({data}) => {
                             <li>
                                 <a className="blue-link" href='https://egghead.io/courses/just-enough-functional-programming-in-javascript'>Just Enough Functional Programming in JavaScript</a>
                             </li>
+                            <li>
+                                <a className="blue-link" href='https://www.udemy.com/course/wordpress-theme-and-plugin-development-course/'>Complete WordPress Theme & Plugin Development Course</a>
+                            </li>
+                        </ul>
+                        <span className='font-black lowercase tracking-tight text-2xl'>2021</span>
+                        <ul className='sm:pl-4 flex flex-col'>
+                            <li>
+                                <a className="blue-link" href='https://egghead.io/courses/the-beginner-s-guide-to-vue-3-1c46da8b'>The Beginner's Guide to Vue 3</a>
+                            </li>
+                            <li>
+                                <a className="blue-link" href='https://www.vuetoronto.com/'>VueConf Toronto</a>
+                            </li>
+                            <li>
+                                <a className="blue-link" href='https://vueschool.io/courses/typescript-fundamentals'>TypeScript Fundamentals</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -83,3 +105,4 @@ export default ({data}) => {
     )
 }
 
+export default About
